6312d55e1fb57ad4e037f64a5b545f98dbafcfae
dianfitrip <dianfitri683@gmail.com>
2024-03-14 10:52:12 +0700

    Bagian Display

--- pertemuan4_PemDas/pertemuan4_PemDas.cpp
+++ pertemuan4_PemDas/pertemuan4_PemDas.cpp
@@ -54,3 +54,16 @@ void insertionsort() {
 	}
 
 }
+
+
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl; 
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl; 
+}
