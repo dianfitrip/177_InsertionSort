2974e3cc5f39e4ec4c66015f7931466ed28f9472
dianfitrip <dianfitri683@gmail.com>
2024-03-14 10:20:45 +0700

    Procedure Input

--- pertemuan4_PemDas/pertemuan4_PemDas.cpp
+++ pertemuan4_PemDas/pertemuan4_PemDas.cpp
@@ -3,3 +3,22 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;	
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+}
