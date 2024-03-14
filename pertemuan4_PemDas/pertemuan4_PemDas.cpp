94640a7a9901e0aacd8225ee555b2ecd57dfa7cf
dianfitrip <dianfitri683@gmail.com>
2024-03-14 10:34:34 +0700

    Bagian Insertion Sort

--- pertemuan4_PemDas/pertemuan4_PemDas.cpp
+++ pertemuan4_PemDas/pertemuan4_PemDas.cpp
@@ -10,7 +10,7 @@ void input() {
 		cin >> n;
 
 		if (n <= 20) {
-			break;	
+			break;
 		}
 		else
 		{
@@ -26,5 +26,25 @@ void input() {
 	{
 		cout << "Data ke-" << (i + 1) << ": ";
 		cin >> arr[i];
+	}
+}
+
+
+void insertionsort() {
+
+	int temp;
+	int i, j;
+
+	for (i + 1; i <= n - 1; i++) {
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
 	}
 }
